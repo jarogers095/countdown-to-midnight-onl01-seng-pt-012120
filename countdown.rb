65666cd7c3812fe7019a248e3feb31efe623f577
James Rogers <jarogers095@gmail.com>
2020-01-26 08:52:11 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -10,7 +10,7 @@ end
 
 def countdown_with_sleep(seconds)
   while seconds > 0 do
-    seconds == 1 ? puts "#{seconds} SECOND" : puts "#{seconds} SECONDS"
+    (seconds == 1) ? puts "#{seconds} SECOND" : puts "#{seconds} SECONDS"
     seconds -= 1
     sleep(1000)
   end
