a01f90230adfa6977f9a1c38cfa2ce6d9ffc71b2
James Rogers <jarogers095@gmail.com>
2020-01-26 08:51:34 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(seconds)
+  while seconds > 0 do
+    seconds == 1 ? puts "#{seconds} SECOND" : puts "#{seconds} SECONDS"
+    seconds -= 1
+  end
+  return "HAPPY NEW YEAR!" 
+end
+
+def countdown_with_sleep(seconds)
+  while seconds > 0 do
+    seconds == 1 ? puts "#{seconds} SECOND" : puts "#{seconds} SECONDS"
+    seconds -= 1
+    sleep(1000)
+  end
+  return "HAPPY NEW YEAR!" 
 end
